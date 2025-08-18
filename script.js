8527635c5fc2bc8bad510638195733b3f6094bf5
Calbars1995 <cschleicher611@gmail.com>
2025-08-18 19:00:06 -0400

    First class constructor

---
+++ script.js
@@ -0,0 +1,10 @@
+class ProductProperties
+{
+    //creating the class with name, price and quantity
+    constructor(name, price, quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
